ddf873984ebc29fd80ef9ed70a9e8778688e8f66
周建华 <zjh96@live.cn>
2020-05-24 11:01:18 +0800

    Update stream helper references.

--- WaveGenerator/Helpers/StreamExtensions.cs
+++ WaveGenerator/Helpers/StreamExtensions.cs
@@ -9,29 +9,25 @@ namespace XstarS.IO
     internal static class StreamExtensions
     {
         /// <summary>
-        /// 从当前流读取指定长度的字节序列，并提升流的位置。
+        /// 从当前流读取指定长度的字节序列到缓冲区，并提升流的位置。
         /// </summary>
         /// <param name="stream">要读取的流。</param>
-        /// <param name="length">要读取的字节序列的长度。</param>
-        /// <returns>读取当前流得到的字节序列。</returns>
-        /// <exception cref="ArgumentNullException">
-        /// <paramref name="stream"/> 为 <see langword="null"/>。</exception>
-        /// <exception cref="ArgumentOutOfRangeException">
-        /// <paramref name="length"/> 为负数。</exception>
-        public static byte[] Read(this Stream stream, int length)
+        /// <param name="buffer">包含读取得到的字节序列的缓冲区。</param>
+        /// <returns>读取到缓冲区的字节序列的长度。</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="stream"/>
+        /// 或 <paramref name="buffer"/> 为 <see langword="null"/>。</exception>
+        public static int Read(this Stream stream, byte[] buffer)
         {
             if (stream is null)
             {
                 throw new ArgumentNullException(nameof(stream));
             }
-            if (length < 0)
+            if (buffer is null)
             {
-                throw new ArgumentOutOfRangeException(nameof(length));
+                throw new ArgumentNullException(nameof(buffer));
             }
 
-            var bytes = new byte[length];
-            stream.Read(bytes, 0, length);
-            return bytes;
+            return stream.Read(buffer, 0, buffer.Length);
         }
 
         /// <summary>
@@ -39,10 +35,11 @@ namespace XstarS.IO
         /// </summary>
         /// <typeparam name="T">非托管数据的类型。</typeparam>
         /// <param name="stream">要读取的流。</param>
-        /// <returns>读取当前流得到的非托管数据。</returns>
+        /// <param name="value">读取得到的非托管数据。</param>
+        /// <returns>是否读取到完整大小的非托管数据。</returns>
         /// <exception cref="ArgumentNullException">
         /// <paramref name="stream"/> 为 <see langword="null"/>。</exception>
-        public static unsafe T Read<T>(this Stream stream)
+        public static unsafe bool Read<T>(this Stream stream, out T value)
             where T : unmanaged
         {
             if (stream is null)
@@ -50,32 +47,35 @@ namespace XstarS.IO
                 throw new ArgumentNullException(nameof(stream));
             }
 
-            var bytes = stream.Read(sizeof(T));
+            var size = sizeof(T);
+            var bytes = new byte[size];
+            var length = stream.Read(bytes);
             fixed (byte* pBytes = bytes)
             {
-                return *(T*)pBytes;
+                value = *(T*)pBytes;
             }
+            return length == size;
         }
 
         /// <summary>
-        /// 向当前流写入指定的字节序列，并提升流的位置。
+        /// 向当前流写入指定的缓冲区中的字节序列，并提升流的位置。
         /// </summary>
         /// <param name="stream">要写入的流。</param>
-        /// <param name="bytes">要写入的字节序列。</param>
+        /// <param name="buffer">包含要写入的字节序列的缓冲区。</param>
         /// <exception cref="ArgumentNullException"><paramref name="stream"/>
-        /// 或 <paramref name="bytes"/> 为 <see langword="null"/>。</exception>
-        public static void Write(this Stream stream, params byte[] bytes)
+        /// 或 <paramref name="buffer"/> 为 <see langword="null"/>。</exception>
+        public static void Write(this Stream stream, byte[] buffer)
         {
             if (stream is null)
             {
                 throw new ArgumentNullException(nameof(stream));
             }
-            if (bytes is null)
+            if (buffer is null)
             {
-                throw new ArgumentNullException(nameof(bytes));
+                throw new ArgumentNullException(nameof(buffer));
             }
 
-            stream.Write(bytes, 0, bytes.Length);
+            stream.Write(buffer, 0, buffer.Length);
         }
 
         /// <summary>
@@ -94,7 +94,8 @@ namespace XstarS.IO
                 throw new ArgumentNullException(nameof(stream));
             }
 
-            var bytes = new byte[sizeof(T)];
+            var size = sizeof(T);
+            var bytes = new byte[size];
             fixed (byte* pBytes = bytes)
             {
                 *(T*)pBytes = value;
