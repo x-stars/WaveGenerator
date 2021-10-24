617b503b813b61db0fd1933b5e19905f845ab455
周建华 <zjh96@live.cn>
2021-10-25 00:25:17 +0800

    Add more surroud wave audio types.

--- WaveGenerator/WaveAudio/WaveChannels.cs
+++ WaveGenerator/WaveAudio/WaveChannels.cs
@@ -14,12 +14,32 @@
         /// </summary>
         Stereo = 2,
         /// <summary>
+        /// 表示 2.1 环绕声。
+        /// </summary>
+        Surround2_1 = 3,
+        /// <summary>
+        /// 表示 3.1 环绕声。
+        /// </summary>
+        Surround3_1 = 4,
+        /// <summary>
+        /// 表示 4.1 环绕声。
+        /// </summary>
+        Surround4_1 = 5,
+        /// <summary>
         /// 表示 5.1 环绕声。
         /// </summary>
-        Surround51 = 6,
+        Surround5_1 = 6,
+        /// <summary>
+        /// 表示 6.1 环绕声。
+        /// </summary>
+        Surround6_1 = 7,
         /// <summary>
         /// 表示 7.1 环绕声。
         /// </summary>
-        Surround71 = 8
+        Surround7_1 = 8,
+        /// <summary>
+        /// 表示 9.1 环绕声。
+        /// </summary>
+        Surround9_1 = 10
     }
 }
