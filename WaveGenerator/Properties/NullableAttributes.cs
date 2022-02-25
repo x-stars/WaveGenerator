6c001bc9fac928931a3060636f616ce50ebd330f
天南十字星 <zjh96@live.cn>
2022-02-25 23:17:45 +0800

    Update project framework, add nullable support.

---
+++ WaveGenerator/Properties/NullableAttributes.cs
@@ -0,0 +1,301 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#if !(NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER)
+namespace System.Diagnostics.CodeAnalysis
+{
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// Specifies that null is allowed as an input even if the corresponding type disallows it.
+    /// </summary>
+    [CompilerGenerated, DebuggerNonUserCode]
+    [AttributeUsage(
+        AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property,
+        Inherited = false)]
+    internal sealed class AllowNullAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AllowNullAttribute"/> class.
+        /// </summary>
+        public AllowNullAttribute()
+        {
+        }
+    }
+
+    /// <summary>
+    /// Specifies that null is disallowed as an input even if the corresponding type allows it.
+    /// </summary>
+    [CompilerGenerated, DebuggerNonUserCode]
+    [AttributeUsage(
+        AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property,
+        Inherited = false)]
+    internal sealed class DisallowNullAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DisallowNullAttribute"/> class.
+        /// </summary>
+        public DisallowNullAttribute()
+        {
+        }
+    }
+
+    /// <summary>
+    /// Specifies that an output may be null even if the corresponding type disallows it.
+    /// </summary>
+    [CompilerGenerated, DebuggerNonUserCode]
+    [AttributeUsage(
+        AttributeTargets.Field | AttributeTargets.Parameter |
+        AttributeTargets.Property | AttributeTargets.ReturnValue,
+        Inherited = false)]
+    internal sealed class MaybeNullAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MaybeNullAttribute"/> class.
+        /// </summary>
+        public MaybeNullAttribute()
+        {
+        }
+    }
+
+    /// <summary>
+    /// Specifies that an output will not be null even if the corresponding type allows it.
+    /// Specifies that an input argument was not null when the call returns.
+    /// </summary>
+    [CompilerGenerated, DebuggerNonUserCode]
+    [AttributeUsage(
+        AttributeTargets.Field | AttributeTargets.Parameter |
+        AttributeTargets.Property | AttributeTargets.ReturnValue,
+        Inherited = false)]
+    internal sealed class NotNullAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NotNullAttribute"/> class.
+        /// </summary>
+        public NotNullAttribute()
+        {
+        }
+    }
+
+    /// <summary>
+    /// Specifies that when a method returns <see cref="ReturnValue"/>,
+    /// the parameter may be null even if the corresponding type disallows it.
+    /// </summary>
+    [CompilerGenerated, DebuggerNonUserCode]
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    internal sealed class MaybeNullWhenAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes the attribute with the specified return value condition.
+        /// </summary>
+        /// <param name="returnValue">The return value condition. 
+        /// If the method returns this value, the associated parameter may be null.</param>
+        public MaybeNullWhenAttribute(bool returnValue)
+        {
+            this.ReturnValue = returnValue;
+        }
+
+        /// <summary>
+        /// Gets the return value condition.
+        /// </summary>
+        /// <returns>The return value condition. 
+        /// If the method returns this value, the associated parameter may be null.</returns>
+        public bool ReturnValue { get; }
+    }
+
+    /// <summary>
+    /// Specifies that when a method returns <see cref="ReturnValue"/>,
+    /// the parameter will not be null even if the corresponding type allows it.
+    /// </summary>
+    [CompilerGenerated, DebuggerNonUserCode]
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    internal sealed class NotNullWhenAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes the attribute with the specified return value condition.
+        /// </summary>
+        /// <param name="returnValue">The return value condition.
+        /// If the method returns this value, the associated parameter will not be null.</param>
+        public NotNullWhenAttribute(bool returnValue)
+        {
+            this.ReturnValue = returnValue;
+        }
+
+        /// <summary>
+        /// Gets the return value condition.
+        /// </summary>
+        /// <returns>The return value condition. 
+        /// If the method returns this value, the associated parameter may be null.</returns>
+        public bool ReturnValue { get; }
+    }
+
+    /// <summary>
+    /// Specifies that the output will be non-null if the named parameter is non-null.
+    /// </summary>
+    [CompilerGenerated, DebuggerNonUserCode]
+    [AttributeUsage(
+        AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue,
+        AllowMultiple = true, Inherited = false)]
+    internal sealed class NotNullIfNotNullAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes the attribute with the associated parameter name.
+        /// </summary>
+        /// <param name="parameterName">The associated parameter name.
+        /// The output will be non-null if the argument to the parameter specified is non-null.</param>
+        public NotNullIfNotNullAttribute(string parameterName)
+        {
+            this.ParameterName = parameterName;
+        }
+
+        /// <summary>
+        /// Gets the associated parameter name.
+        /// </summary>
+        /// <returns>The associated parameter name.
+        /// The output will be non-null if the argument to the parameter specified is non-null.</returns>
+        public string ParameterName { get; }
+    }
+
+    /// <summary>
+    /// Applied to a method that will never return under any circumstance.
+    /// </summary>
+    [CompilerGenerated, DebuggerNonUserCode]
+    [AttributeUsage(AttributeTargets.Method, Inherited = false)]
+    internal sealed class DoesNotReturnAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DoesNotReturnAttribute"/> class.
+        /// </summary>
+        public DoesNotReturnAttribute()
+        {
+        }
+    }
+
+    /// <summary>
+    /// Specifies that the method will not return
+    /// if the associated Boolean parameter is passed the specified value.
+    /// </summary>
+    [CompilerGenerated, DebuggerNonUserCode]
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    internal sealed class DoesNotReturnIfAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes the attribute with the specified parameter value.
+        /// </summary>
+        /// <param name="parameterValue">The condition parameter value.
+        /// Code after the method will be considered unreachable by diagnostics
+        /// if the argument to the associated parameter matches this value.</param>
+        public DoesNotReturnIfAttribute(bool parameterValue)
+        {
+            this.ParameterValue = parameterValue;
+        }
+
+        /// <summary>
+        /// Gets the condition parameter value.
+        /// </summary>
+        /// <returns>The condition parameter value.
+        /// Code after the method will be considered unreachable by diagnostics
+        /// if the argument to the associated parameter matches this value.</returns>
+        public bool ParameterValue { get; }
+    }
+}
+#endif
+
+#if !NET5_0_OR_GREATER
+namespace System.Diagnostics.CodeAnalysis
+{
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// Specifies that the method or property will ensure
+    /// that the listed field and property members have not-null values.
+    /// </summary>
+    [CompilerGenerated, DebuggerNonUserCode]
+    [AttributeUsage(
+        AttributeTargets.Method | AttributeTargets.Property,
+        Inherited = false, AllowMultiple = true)]
+    internal sealed class MemberNotNullAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes the attribute with a field or property member.
+        /// </summary>
+        /// <param name="member">
+        /// The field or property member that is promised to be not-null.</param>
+        public MemberNotNullAttribute(string member)
+        {
+            this.Members = new[] { member };
+        }
+
+        /// <summary>
+        /// Initializes the attribute with the list of field and property members.
+        /// </summary>
+        /// <param name="members">
+        /// The list of field and property members that are promised to be not-null.</param>
+        public MemberNotNullAttribute(params string[] members)
+        {
+            this.Members = members;
+        }
+
+        /// <summary>
+        /// Gets field or property member names.
+        /// </summary>
+        /// <returns>
+        /// The list of field and property members that are promised to be not-null.</returns>
+        public string[] Members { get; }
+    }
+
+    /// <summary>
+    /// Specifies that the method or property will ensure
+    /// that the listed field and property members have not-null values
+    /// when returning with the specified return value condition.
+    /// </summary>
+    [CompilerGenerated, DebuggerNonUserCode]
+    [AttributeUsage(
+        AttributeTargets.Method | AttributeTargets.Property,
+        Inherited = false, AllowMultiple = true)]
+    internal sealed class MemberNotNullWhenAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes the attribute with
+        /// the specified return value condition and a field or property member.
+        /// </summary>
+        /// <param name="returnValue">The return value condition.
+        /// If the method returns this value, the associated parameter will not be null.</param>
+        /// <param name="member">
+        /// The field or property member that is promised to be not-null.</param>
+        public MemberNotNullWhenAttribute(bool returnValue, string member)
+        {
+            this.ReturnValue = returnValue;
+            this.Members = new[] { member };
+        }
+
+        /// <summary>
+        /// Initializes the attribute with
+        /// the specified return value condition and list of field and property members.
+        /// </summary>
+        /// <param name="returnValue">The return value condition.
+        /// If the method returns this value, the associated parameter will not be null.</param>
+        /// <param name="members">
+        /// The list of field and property members that are promised to be not-null.</param>
+        public MemberNotNullWhenAttribute(bool returnValue, params string[] members)
+        {
+            this.ReturnValue = returnValue;
+            this.Members = members;
+        }
+
+        /// <summary>
+        /// Gets the return value condition.
+        /// </summary>
+        /// <returns>The return value condition.
+        /// If the method returns this value, the associated parameter will not be null.</returns>
+        public bool ReturnValue { get; }
+
+        /// <summary>
+        /// Gets field or property member names.
+        /// </summary>
+        /// <returns>
+        /// The list of field and property members that are promised to be not-null.</returns>
+        public string[] Members { get; }
+    }
+}
+#endif
---
+++ WaveGenerator/Properties/RangeForeach.cs
@@ -0,0 +1,155 @@
+﻿// Copyright (c) 2022 XstarS
+// This file is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+// Provide the range-foreach syntax for C# 9.0 or higher.
+// Requires: Framework >= 4.0 || Core || Standard.
+// Reference this file to write foreach-loops like this:
+//   foreach (var index in 0..100) { /* ... */ }
+// which is equivalent to the legacy for-loop below:
+//   for (int i = 0; i < 100; i++) { /* ... */ }
+// NOTE: use '^' to represent negative numbers,
+//       e.g. ^100..0 (instead of -100..0).
+
+#nullable disable
+
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[CompilerGenerated, DebuggerNonUserCode]
+[EditorBrowsable(EditorBrowsableState.Never)]
+internal static class RangeEnumerable
+{
+    public static RangeEnumerator GetEnumerator(this Range range)
+    {
+        return new RangeEnumerator(range);
+    }
+}
+
+namespace System
+{
+    [CompilerGenerated, DebuggerNonUserCode]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    internal struct RangeEnumerator
+    {
+        private int CurrentIndex;
+
+        private readonly int EndIndex;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public RangeEnumerator(Range range)
+        {
+            var pair = new IndexPair() { Range = range };
+            int start = pair.StartIndex, end = pair.EndIndex;
+            this.CurrentIndex = start - (start >> 31) - 1;
+            this.EndIndex = end - (end >> 31);
+        }
+
+        public int Current => this.CurrentIndex;
+
+        public bool MoveNext() => ++this.CurrentIndex < this.EndIndex;
+
+        [StructLayout(LayoutKind.Explicit)]
+        [CompilerGenerated, DebuggerNonUserCode]
+        private struct IndexPair
+        {
+            [FieldOffset(0)] public Range Range;
+            [FieldOffset(0)] public int StartIndex;
+            [FieldOffset(4)] public int EndIndex;
+        }
+    }
+}
+
+#if !(NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER)
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// NOTE: some APIs have been removed for code size and compatibility reasons.
+
+namespace System
+{
+    [CompilerGenerated, DebuggerNonUserCode]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    internal readonly struct Index : IEquatable<Index>
+    {
+        private readonly int _value;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Index(int value, bool fromEnd = false)
+        {
+            if (value < 0) { throw Index.ValueOutOfRange(); }
+            this._value = fromEnd ? ~value : value;
+        }
+
+        private Index(int value) { this._value = value; }
+
+        public static Index Start => new Index(0);
+
+        public static Index End => new Index(~0);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Index FromStart(int value) =>
+            (value < 0) ? throw Index.ValueOutOfRange() : new Index(value);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Index FromEnd(int value) =>
+            (value < 0) ? throw Index.ValueOutOfRange() : new Index(~value);
+
+        public int Value => (this._value < 0) ? ~this._value : this._value;
+
+        public bool IsFromEnd => this._value < 0;
+
+        //[MethodImpl(MethodImplOptions.AggressiveInlining)]
+        //public int GetOffset(int length) { ... }
+
+        public override bool Equals(object value) =>
+            (value is Index other) && (this._value == other._value);
+
+        public bool Equals(Index other) => this._value == other._value;
+
+        public override int GetHashCode() => this._value;
+
+        public static implicit operator Index(int value) => Index.FromStart(value);
+
+        public override string ToString() =>
+            this.IsFromEnd ? this.ToStringFromEnd() : ((uint)this.Value).ToString();
+
+        private string ToStringFromEnd() => "^" + this.Value.ToString();
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static ArgumentOutOfRangeException ValueOutOfRange() =>
+            new ArgumentOutOfRangeException("value", "Non-negative number required.");
+    }
+
+    [CompilerGenerated, DebuggerNonUserCode]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    internal readonly struct Range : IEquatable<Range>
+    {
+        public Index Start { get; }
+
+        public Index End { get; }
+
+        public Range(Index start, Index end) { this.Start = start; this.End = end; }
+
+        public override bool Equals(object value) =>
+            (value is Range other) && this.Start.Equals(other.Start) && this.End.Equals(other.End);
+
+        public bool Equals(Range other) => this.Start.Equals(other.Start) && this.End.Equals(other.End);
+
+        public override int GetHashCode() => this.Start.GetHashCode() * 31 + this.End.GetHashCode();
+
+        public override string ToString() => this.Start.ToString() + ".." + this.End.ToString();
+
+        public static Range StartAt(Index start) => new Range(start, Index.End);
+
+        public static Range EndAt(Index end) => new Range(Index.Start, end);
+
+        public static Range All => new Range(Index.Start, Index.End);
+
+        //[MethodImpl(MethodImplOptions.AggressiveInlining)]
+        //public (int Offset, int Length) GetOffsetAndLength(int length) { ... }
+    }
+}
+#endif
